0ecb0325351a69fb63ee95505d5319f66222b594
hectoramadorglez <hectoramador2005@gmail.com>
2024-10-10 19:16:26 +0200

    weq

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class Player : MonoBehaviour
@@ -8,6 +9,9 @@ public class Player : MonoBehaviour
     Rigidbody rb;
     [SerializeField] int fuerza;
     Vector3 miVector = new Vector3(0,1,0);
+    int puntuacion;
+    [SerializeField] TMP_Text  textoPuntuacion;
+
     void Start()
     {
        rb = GetComponent<Rigidbody>();
@@ -19,17 +23,31 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+       
+
+
+    }
+    private void FixedUpdate()
+    {
         float h = Input.GetAxisRaw("Horizontal");
         float v = Input.GetAxisRaw("Vertical");
-        if (Input.GetKeyDown(KeyCode.Space)) 
+        if (Input.GetKeyDown(KeyCode.Space))
         {
-            rb.AddForce(new Vector3(0,1,0) * fuerza, ForceMode.Impulse);
-            
-        
-        
+            rb.AddForce(new Vector3(0, 1, 0) * fuerza, ForceMode.Impulse);
+
+
+
         }
-        rb.AddForce (new Vector3(h,0,0)* fuerza, ForceMode.Force);
+        rb.AddForce(new Vector3(h, 0, 0) * fuerza, ForceMode.Force);
         rb.AddForce(new Vector3(0, 0, v) * fuerza, ForceMode.Force);
+
+
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        puntuacion += 10;
+        textoPuntuacion.SetText("Score: "+ puntuacion);
+
 
 
     }
---
+++ Assets/Scripts/Rodillo.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rodillo : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private Rigidbody rb;
+    [SerializeField]   int velocidad;
+    [SerializeField] int fuerza;
+    [SerializeField] 
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.AddTorque(new Vector3 (0,0,0) * fuerza, ForceMode.Impulse);
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+
+       
+    }
+}
