ed1c67d6bcb732d5f7c5fab9174487f29efb6b49
hectoramadorglez <hectoramador2005@gmail.com>
2024-10-20 23:20:48 +0200

    gfdfgdhjk

--- Assets/Scripts/Paredcubos.cs
+++ Assets/Scripts/Paredcubos.cs
@@ -1,18 +1,46 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering;
 
 public class Paredcubos : MonoBehaviour
 {
     // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    private bool iniciarTimer;
+    private  float timer = 0f;
+    [SerializeField] private Rigidbody[] rbs;
 
     // Update is called once per frame
     void Update()
     {
+        if (iniciarTimer == true)
+        {
+            timer += 1 * Time.unscaledDeltaTime;
+            if (timer >= 2)
+            {
+
+                Time.timeScale = 1;
+                for (int i = 0;i < rbs.Length; i++)
+                {
+                    rbs[i].useGravity = true;
+                
+                }         
+
+            }
+
+
+        }
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player")) 
+        {
+
+            Time.timeScale = 0.1f;
+            iniciarTimer = true;
         
+        }
+
+
     }
 }
